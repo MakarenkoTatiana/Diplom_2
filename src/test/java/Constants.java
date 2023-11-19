15fa496f5fc697fe5349ac698f53a96d24fd2d5b
makarenko <t.makarenko@cft.ru>
2023-11-19 22:53:08 +0700

    Заготовки тестов

---
+++ src/test/java/Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+    public static final String email = "makar_test@gmail.ru";
+    public static final String pass = "MakarTest12345";
+    public static final String incorrectPass = "Test12345";
+    public static final String userName = "MakarTestUser";
+    public static final String baseUri = "https://stellarburgers.nomoreparties.site";
+    public static final String json = "{\"email\": \"" + email + "\", \"password\": \"" + pass + "\", \"name\": \"" + userName + "\"}";
+    public static final String incorrectJson = "{\"email\": \"" + email + "\", \"password\": \"" + pass +"\", \"name\": \"\"}";
+    public static final String incorrectLoginJson = "{\"email\": \"" + email + "\", \"password\": \"" + incorrectPass +"\", \"name\": \"\"}";
+    public static final String loginJson = "{\"email\": \"" + email + "\", \"password\": \"" + pass + "\"}";
+
+}
---
+++ src/test/java/CreateUserTest.java
@@ -0,0 +1,72 @@
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateUserTest {
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = Constants.baseUri;
+    }
+    String accessToken;
+    Response response;
+
+    @Test
+    public void registerUniqUser() {
+        // POST запрос на регистрацию
+        sendPostRequestAuthRegister(Constants.json);
+        accessToken = response.then().extract().path("accessToken");
+        response.then().assertThat()
+                // статус ответа
+                .statusCode(200);
+        sendDeleteRequestAuthUser(accessToken);
+    }
+
+    @Test
+    public void registerExistsUser() {
+        // POST запрос на регистрацию
+        sendPostRequestAuthRegister(Constants.json);
+        accessToken = response.then().extract().path("accessToken");
+        response.then()
+                // статус ответа
+                .statusCode(200);
+        // POST запрос на регистрацию
+        sendPostRequestAuthRegister(Constants.json);
+        response.then().assertThat()
+                // статус ответа
+                .statusCode(403);
+        sendDeleteRequestAuthUser(accessToken);
+    }
+
+    @Test
+    public void registerUserWithIncorrectData() {
+        // POST запрос на регистрацию
+        sendPostRequestAuthRegister(Constants.incorrectJson);
+        response.then()
+                // статус ответа
+                .statusCode(403);
+    }
+
+    @Step("Send POST request to /api/auth/register")
+    public Response sendPostRequestAuthRegister(String jsonString) {
+        response = given()
+                .header("Content-type", "application/json")
+                .body(jsonString)
+                .when()
+                .post("/api/auth/register");
+        return response;
+    }
+
+    @Step("Send DELETE request to /api/auth/user")
+    public void sendDeleteRequestAuthUser(String token) {
+        given()
+             .header("Content-type", "application/json")
+             .header("Authorization", token)
+             .when()
+             .delete("/api/auth/user");
+    }
+}
