e2d5c7ab0d0ef4fa82f10ba8838c082f7191eb08
makarenko <t.makarenko@cft.ru>
2023-11-24 00:17:05 +0700

    Update2 after CR

--- src/test/java/LoginUserTest.java
+++ src/test/java/LoginUserTest.java
@@ -1,14 +1,17 @@
 import model.Constants;
+import model.User;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static model.Constants.*;
 
 public class LoginUserTest extends TestBase {
     String refreshToken;
 
     @Test
     public void loginExistsUser() {
-        sendPostRequestAuthLogin(Constants.LOGIN_JSON);
+        User userLogin = new User(EMAIL, PASS);
+        sendPostRequestAuthLogin(userLogin);
         refreshToken = loginResponse.then().extract().path("refreshToken");
         loginResponse.then().statusCode(200);
         sendPostRequestAuthLogout(refreshToken);
@@ -16,7 +19,8 @@ public class LoginUserTest extends TestBase {
 
     @Test
     public void loginIncorrectUser() {
-        sendPostRequestAuthLogin(Constants.INCORRECT_LOGIN_JSON);
+        User userIncorrect = new User(EMAIL, INCORRECT_PASS);
+        sendPostRequestAuthLogin(userIncorrect);
         loginResponse.then().statusCode(401);
     }
 }
